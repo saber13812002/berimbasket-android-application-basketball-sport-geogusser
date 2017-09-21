be40167aa1ce064d4271974450475016c78c047f
Mahdi <m.javaherisaber@gmail.com>
2017-09-21 12:57:38 +0430

    Set web services url to support section
    about_us, help, terms_and_services, changelog urls
    which goes to custom tabs

--- app/src/main/java/ir/berimbasket/app/activity/fragment/FragmentSettings.java
+++ app/src/main/java/ir/berimbasket/app/activity/fragment/FragmentSettings.java
@@ -10,14 +10,21 @@ import ir.berimbasket.app.util.SendTo;
 
 public class FragmentSettings extends PreferenceFragmentCompat implements Preference.OnPreferenceClickListener {
 
+    private final static String URL_PREFERENCE_HELP = "http://berimbasket.ir/help";
+    private final static String URL_PREFERENCE_TERMS_AND_SERVICES = "http://berimbasket.ir/terms";
+    private final static String URL_PREFERENCE_ABOUT_US = "http://berimbasket.ir/about";
+    private final static String URL_PREFERENCE_CHANGE_LOG = "http://berimbasket.ir/changelog";
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preference_settings);
 
         // FIXME: 9/20/2017 read keys from strings.xml
+        Preference help = findPreference("help");
         Preference aboutUs = findPreference("about_us");
         Preference terms = findPreference("terms_and_services");
         Preference changeLog = findPreference("change_log");
+        help.setOnPreferenceClickListener(this);
         aboutUs.setOnPreferenceClickListener(this);
         terms.setOnPreferenceClickListener(this);
         changeLog.setOnPreferenceClickListener(this);
@@ -27,16 +34,18 @@ public class FragmentSettings extends PreferenceFragmentCompat implements Prefer
     public boolean onPreferenceClick(Preference preference) {
         String key = preference.getKey();
         // FIXME: 9/20/2017 read keys from strings.xml
-        // TODO: 9/20/2017 set correct webservice url
         switch (key) {
+            case "help" :
+                SendTo.sendToCustomTab(getActivity(), URL_PREFERENCE_HELP);
+                return true;
             case "about_us" :
-                SendTo.sendToCustomTab(getActivity(), "https://www.google.com/search?q=about+us");
+                SendTo.sendToCustomTab(getActivity(), URL_PREFERENCE_ABOUT_US);
                 return true;
             case "terms_and_services" :
-                SendTo.sendToCustomTab(getActivity(), "https://www.google.com/search?q=terms+and+services");
+                SendTo.sendToCustomTab(getActivity(), URL_PREFERENCE_TERMS_AND_SERVICES);
                 return true;
             case "change_log" :
-                SendTo.sendToCustomTab(getActivity(), "https://www.google.com/search?q=changelog");
+                SendTo.sendToCustomTab(getActivity(), URL_PREFERENCE_CHANGE_LOG);
                 return true;
         }
         return false;
