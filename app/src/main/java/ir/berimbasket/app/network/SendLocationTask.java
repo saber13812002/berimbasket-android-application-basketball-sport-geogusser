307b5391d845cbd74dffccfb1ea5c220219c20c6
Mahdi <m.javaherisaber@gmail.com>
2017-09-22 08:31:26 +0330

    Set web service to send user location on app startup

--- app/src/main/java/ir/berimbasket/app/network/SendLocationTask.java
+++ app/src/main/java/ir/berimbasket/app/network/SendLocationTask.java
@@ -3,6 +3,7 @@ package ir.berimbasket.app.network;
 import android.os.AsyncTask;
 
 import ir.berimbasket.app.entity.EntityLocation;
+import ir.berimbasket.app.json.HttpFunctions;
 
 /**
  * Created by Mahdi on 9/20/2017.
@@ -10,9 +11,14 @@ import ir.berimbasket.app.entity.EntityLocation;
  */
 
 public class SendLocationTask extends AsyncTask<EntityLocation, Void, Void> {
+
+    private final static String URL_SET_LOCATION = "https://berimbasket.ir/bball/setLoc.php";
     @Override
     protected Void doInBackground(EntityLocation... params) {
-        // TODO: 9/20/2017 send user location with specified webservice here
+        HttpFunctions httpFuncs = new HttpFunctions(HttpFunctions.RequestType.GET);
+        String url = URL_SET_LOCATION + String.format("?token=jkhfgkljhasfdlkh&lat=%s&long=%s&title=title"
+                , params[0].getLatitude(), params[0].getLongitude());
+        httpFuncs.makeServiceCall(url);
         return null;
     }
 }
