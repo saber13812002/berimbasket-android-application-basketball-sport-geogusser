b07b8dc37314fc54e153d629b0d9e9dd91798192
Mahdi <m.javaherisaber@gmail.com>
2017-12-18 00:19:16 +0330

    Set default location of map to Tehran

--- app/src/main/java/ir/berimbasket/app/util/PrefManager.java
+++ app/src/main/java/ir/berimbasket/app/util/PrefManager.java
@@ -114,7 +114,9 @@ public class PrefManager extends SecureSharedPreferences {
 
     public String getSettingsPrefStateList() {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
-        return pref.getString(context.getString(R.string.key_pref_state_list), "0a0");  // 0a0 means based on phone location
+        // FIXME: 12/18/2017 set based on phone location
+//        return pref.getString(context.getString(R.string.key_pref_state_list), "0a0");  // 0a0 means based on phone location
+        return pref.getString(context.getString(R.string.key_pref_state_list), "35.6891980a51.3889740");
     }
 
     public void putSettingsPrefStateList(String value) {
