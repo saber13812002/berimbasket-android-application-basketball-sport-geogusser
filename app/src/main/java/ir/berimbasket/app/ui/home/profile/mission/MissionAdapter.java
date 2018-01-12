825137ab778fb4c8ad7950a1571103d41ded2107
Mahdi <m.javaherisaber@gmail.com>
2018-01-12 11:42:37 +0330

    Replace mission api with Retrofit

--- app/src/main/java/ir/berimbasket/app/ui/home/profile/mission/MissionAdapter.java
+++ app/src/main/java/ir/berimbasket/app/ui/home/profile/mission/MissionAdapter.java
@@ -1,10 +1,6 @@
 package ir.berimbasket.app.ui.home.profile.mission;
 
 import android.content.Context;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.support.customtabs.CustomTabsIntent;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,27 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.List;
 
-import co.ronash.pushe.Pushe;
 import ir.berimbasket.app.R;
-import ir.berimbasket.app.data.entity.EntityMission;
-import ir.berimbasket.app.data.network.HttpFunctions;
-import ir.berimbasket.app.data.pref.PrefManager;
+import ir.berimbasket.app.data.network.model.Mission;
 
 /**
  * Created by mohammad hosein on 7/22/2017.
  */
 
-public class MissionAdapter extends RecyclerView.Adapter<MissionAdapter.MissionViewHolder> {
+class MissionAdapter extends RecyclerView.Adapter<MissionAdapter.MissionViewHolder> {
 
     private final Context context;
-    private final ArrayList<EntityMission> missionList;
+    private final List<Mission> dataSource;
 
-    public MissionAdapter(ArrayList<EntityMission> xpList, Context context) {
-        this.missionList = xpList;
+    MissionAdapter(List<Mission> xpList, Context context) {
+        this.dataSource = xpList;
         this.context = context;
     }
 
@@ -40,18 +32,17 @@ public class MissionAdapter extends RecyclerView.Adapter<MissionAdapter.MissionV
     public MissionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.item_mission, parent, false);
-        MissionViewHolder missionViewHolder = new MissionViewHolder(view);
-        return missionViewHolder;
+        return new MissionViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final MissionViewHolder holder, final int position) {
 
-        holder.txtMissionTitle.setText(missionList.get(holder.getLayoutPosition()).getTitle());
-        holder.txtMissionBadge.setText(String.valueOf(missionList.get(holder.getLayoutPosition()).getLevel()));
-        holder.txtMissionScore.setText(String.valueOf(missionList.get(holder.getLayoutPosition()).getScore())
+        holder.txtMissionTitle.setText(dataSource.get(position).getTitle());
+        holder.txtMissionBadge.setText(String.valueOf(dataSource.get(position).getLevel()));
+        holder.txtMissionScore.setText(String.valueOf(dataSource.get(position).getScore())
                 + " " + context.getString(R.string.adapter_mission_point) + " ");
-        if (missionList.get(holder.getLayoutPosition()).getIsLock() != 0) {
+        if (dataSource.get(position).getLock() != 0) {
             holder.imgMissionLock.setVisibility(View.VISIBLE);
             holder.txtMissionScore.setVisibility(View.GONE);
 
@@ -63,20 +54,20 @@ public class MissionAdapter extends RecyclerView.Adapter<MissionAdapter.MissionV
         holder.cardMissionItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (missionList.get(holder.getLayoutPosition()).getIsLock() == 0)
-                    new RegisterMissionToUser(new AsyncResponse() {
-
-                        @Override
-                        public void processFinish(String output) {
-                            if (!output.equals("")) {
-                                // TODO: 04/10/2017 Uncomment this part of code when isDone in missionDoneUrl added
-//                            missionList.get(holder.getLayoutPosition()).setIsLock(1);
-//                            holder.imgMissionLock.setVisibility(View.VISIBLE);
-//                            holder.txtMissionScore.setVisibility(View.INVISIBLE);
-//                            notifyItemChanged(holder.getLayoutPosition());
-                            }
-                        }
-                    }).execute(missionList.get(holder.getLayoutPosition()));
+//                if (dataSource.get(holder.getLayoutPosition()).getLock() == 0)
+//                    new RegisterMissionToUser(new AsyncResponse() {
+//
+//                        @Override
+//                        public void processFinish(String output) {
+//                            if (!output.equals("")) {
+//                                // TODO: 04/10/2017 Uncomment this part of code when isDone in missionDoneUrl added
+////                            dataSource.get(holder.getLayoutPosition()).setIsLock(1);
+////                            holder.imgMissionLock.setVisibility(View.VISIBLE);
+////                            holder.txtMissionScore.setVisibility(View.INVISIBLE);
+////                            notifyItemChanged(holder.getLayoutPosition());
+//                            }
+//                        }
+//                    }).execute(dataSource.get(holder.getLayoutPosition()));
             }
         });
     }
@@ -84,7 +75,7 @@ public class MissionAdapter extends RecyclerView.Adapter<MissionAdapter.MissionV
 
     @Override
     public int getItemCount() {
-        return missionList.size();
+        return dataSource.size();
     }
 
     class MissionViewHolder extends RecyclerView.ViewHolder {
@@ -95,82 +86,22 @@ public class MissionAdapter extends RecyclerView.Adapter<MissionAdapter.MissionV
 
         MissionViewHolder(View itemView) {
             super(itemView);
-            this.txtMissionTitle = (TextView) itemView.findViewById(R.id.txtMissionTitle);
-            this.txtMissionBadge = (TextView) itemView.findViewById(R.id.txtMissionBadge);
-            this.txtMissionScore = (TextView) itemView.findViewById(R.id.txtMissionScore);
-            this.imgMissionLock = (ImageView) itemView.findViewById(R.id.imgMissionLock);
-            cardMissionItem = (CardView) itemView.findViewById(R.id.cardMissionItem);
+            this.txtMissionTitle = itemView.findViewById(R.id.txtMissionTitle);
+            this.txtMissionBadge = itemView.findViewById(R.id.txtMissionBadge);
+            this.txtMissionScore = itemView.findViewById(R.id.txtMissionScore);
+            this.imgMissionLock = itemView.findViewById(R.id.imgMissionLock);
+            cardMissionItem = itemView.findViewById(R.id.cardMissionItem);
 
         }
     }
 
-    // TODO: 12/21/2017 @mohammad1444 i don't know about this, when you saw this message tell me what it is
-    private final String MISSION_DONE_URL = "berimbasket.ir/bball/www/setMissionDone.php?";
-
-    private class RegisterMissionToUser extends AsyncTask<EntityMission, Void, String> {
-
-        @Override
-        protected String doInBackground(EntityMission... entityMissions) {
-            EntityMission entityMission = entityMissions[0];
-            HttpFunctions httpFunctions = new HttpFunctions(HttpFunctions.RequestType.GET);
-            String jsonStr = httpFunctions.makeServiceCall(completeMissionDoneUrl(entityMission.getId()));
-
-            // FIXME: 04/10/2017 Json String Doesn't return anything, call server admin to fix it
-//            if (jsonStr != null) {
-//                try {
-//                    JSONArray missionDoneResult = new JSONArray(jsonStr);
-            if (true) {
-                return entityMission.getLink();
-            } else {
-                return "";
-            }
-//                } catch (final JSONException e) {
-//                    Log.i("jsonError", e.toString());
-//                }
-//            }
-//            return "";
-
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            if (!result.equals("")) {
-                setMissionDone(result);
-            } else {
-                Toast.makeText(context, context.getString(R.string.adapter_mission_toast_set_mission_error), Toast.LENGTH_LONG).show();
-            }
-            delegate.processFinish(result);
-        }
-
-        private String completeMissionDoneUrl(int missionId) {
-            PrefManager pref = new PrefManager(context);
-            String pusheId = Pushe.getPusheId(context);
-            String userName = pref.getUserName();
-            return MISSION_DONE_URL + "id=" + missionId + "&token=asf" + "&user=" + userName + "&pusheid=" + pusheId + "&username=" + userName;
-        }
-
-        private void setMissionDone(String missionLink) {
-            Uri missionUri = Uri.parse(missionLink);
-            CustomTabsIntent.Builder intentBuilder = new CustomTabsIntent.Builder();
-            intentBuilder.setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary));
-            intentBuilder.setSecondaryToolbarColor(ContextCompat.getColor(context, R.color.colorPrimaryDark));
-            CustomTabsIntent customTabsIntent = intentBuilder.build();
-            customTabsIntent.launchUrl(context, missionUri);
-        }
-
-
-        AsyncResponse delegate = null;
-
-        RegisterMissionToUser(AsyncResponse delegate) {
-            this.delegate = delegate;
-        }
-
-    }
-
-    interface AsyncResponse {
-        void processFinish(String output);
-    }
-
+//    private void setMissionDone(String missionLink) {
+//        Uri missionUri = Uri.parse(missionLink);
+//        CustomTabsIntent.Builder intentBuilder = new CustomTabsIntent.Builder();
+//        intentBuilder.setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary));
+//        intentBuilder.setSecondaryToolbarColor(ContextCompat.getColor(context, R.color.colorPrimaryDark));
+//        CustomTabsIntent customTabsIntent = intentBuilder.build();
+//        customTabsIntent.launchUrl(context, missionUri);
+//    }
 
 }
--- app/src/main/java/ir/berimbasket/app/ui/home/profile/mission/MissionFragment.java
+++ app/src/main/java/ir/berimbasket/app/ui/home/profile/mission/MissionFragment.java
@@ -1,39 +1,34 @@
 package ir.berimbasket.app.ui.home.profile.mission;
 
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
+import java.net.HttpURLConnection;
+import java.util.List;
 
 import co.ronash.pushe.Pushe;
 import ir.berimbasket.app.R;
-import ir.berimbasket.app.data.entity.EntityMission;
-import ir.berimbasket.app.data.network.HttpFunctions;
+import ir.berimbasket.app.data.network.WebApiClient;
+import ir.berimbasket.app.data.network.model.Mission;
 import ir.berimbasket.app.data.pref.PrefManager;
 import ir.berimbasket.app.util.AnalyticsHelper;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MissionFragment extends Fragment {
-
-    private String MISSION_URL = "http://berimbasket.ir/bball/getMission.php";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_missions, container, false);
-        new GetMissions().execute();
+        initMissions();
         return view;
     }
 
@@ -44,8 +39,8 @@ public class MissionFragment extends Fragment {
         AnalyticsHelper.getInstance().trackScreenView(getContext(), this.getClass().getSimpleName());
     }
 
-    private void initRecyclerMission(View view, ArrayList<EntityMission> missionList) {
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerMissions);
+    private void initRecyclerMission(View view, List<Mission> missionList) {
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerMissions);
         recyclerView.setNestedScrollingEnabled(false);
         MissionAdapter adapterPlayer = new MissionAdapter(missionList, view.getContext());
         recyclerView.setAdapter(adapterPlayer);
@@ -58,88 +53,26 @@ public class MissionFragment extends Fragment {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
-    ArrayList<EntityMission> missionList = new ArrayList<>();
-
-    private class GetMissions extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            HttpFunctions sh = new HttpFunctions(HttpFunctions.RequestType.GET);
-
-            // Making a request to url and getting response
-            String jsonStr = sh.makeServiceCall(completeMissionUrl());
-            if (jsonStr != null) {
-                try {
-                    // Getting JSON Array node
-                    JSONArray locations = new JSONArray(jsonStr);
-
-                    // looping through All Contacts
-                    for (int i = 0; i < locations.length(); i++) {
-                        JSONObject c = locations.getJSONObject(i);
-
-                        String id = c.getString("id");
-                        String title = c.getString("title");
-                        String link = c.getString("link");
-                        String score = c.getString("score");
-                        String level = c.getString("level");
-                        String lock = c.getString("lock");
-
-                        Log.i("name", String.valueOf(Integer.parseInt(level)));
-
-                        EntityMission entityMission = new EntityMission();
-
-                        entityMission.setId(id != "null" ? Integer.parseInt(id) : -1);
-                        entityMission.setTitle(title);
-                        entityMission.setLink(link);
-                        entityMission.setScore(score != "null" ? Integer.parseInt(score) : -1);
-                        entityMission.setLevel(level != "null" ? Integer.parseInt(level) : -1);
-                        entityMission.setIsLock(lock != "null" ? Integer.parseInt(lock) : -1);
-
-                        missionList.add(entityMission);
-
-
-                    }
-                } catch (final JSONException e) {
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getActivity(),
-                                    "Json parsing error: " + e.getMessage(),
-                                    Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    });
-
-                }
-            } else {
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                    }
-                });
-
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            initRecyclerMission(getView(), missionList);
-        }
-    }
-
-    private String completeMissionUrl() {
+    private void initMissions() {
         String pusheId = Pushe.getPusheId(getContext());
         String userName = new PrefManager(getContext()).getUserName();
-        String urlParams = String.format("pusheid=%s&username=%s", pusheId, userName);
-        return MISSION_URL + "?user=" + userName + "&" + urlParams;
+        WebApiClient.getMissionApi().getMissions(userName, userName, pusheId).enqueue(new Callback<List<Mission>>() {
+            @Override
+            public void onResponse(Call<List<Mission>> call, Response<List<Mission>> response) {
+                if (response.code() == HttpURLConnection.HTTP_OK) {
+                    List<Mission> missions = response.body();
+                    if (missions != null && getView() != null) {
+                        initRecyclerMission(getView(), missions);
+                    }
+                } else {
+                    // http call with incorrect params or other network error
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Mission>> call, Throwable t) {
+
+            }
+        });
     }
 }
