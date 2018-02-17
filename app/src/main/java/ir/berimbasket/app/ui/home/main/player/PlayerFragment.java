e5f3d3b1c61c7361d58093a3051a4984f38b4eac
Mahdi <m.javaherisaber@gmail.com>
2018-02-17 12:24:38 +0330

    Fix player list circular progress bar visibility when loading more

--- app/src/main/java/ir/berimbasket/app/ui/home/main/player/PlayerFragment.java
+++ app/src/main/java/ir/berimbasket/app/ui/home/main/player/PlayerFragment.java
@@ -101,7 +101,6 @@ public class PlayerFragment extends Fragment implements PlayerAdapter.PlayerList
         String pusheId = Pushe.getPusheId(getContext());
         String userName = new PrefManager(getContext()).getUserName();
         String lang = LocaleManager.getLocale(getContext()).getLanguage();
-        circularProgressBar.setVisibility(View.VISIBLE);
         WebApiClient.getPlayerApi().getPlayersV2(from, num, "json", pusheId, userName, lang).enqueue(new Callback<List<Player>>() {
             @Override
             public void onResponse(Call<List<Player>> call, Response<List<Player>> response) {
