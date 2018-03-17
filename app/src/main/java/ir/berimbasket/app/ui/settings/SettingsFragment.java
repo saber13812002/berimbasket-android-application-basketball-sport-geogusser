43d702020ec4a46e0c0399184bc5d4736a774f0b
Mahdi <m.javaherisaber@gmail.com>
2018-03-17 22:42:06 +0330

    Add product flavors to project and separate myket from bazaar

--- app/src/main/java/ir/berimbasket/app/ui/settings/SettingsFragment.java
+++ app/src/main/java/ir/berimbasket/app/ui/settings/SettingsFragment.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.widget.Toast;
 
+import ir.berimbasket.app.BuildConfig;
 import ir.berimbasket.app.R;
 import ir.berimbasket.app.data.pref.PrefManager;
 import ir.berimbasket.app.ui.contact.DeveloperContactActivity;
@@ -23,7 +25,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Prefer
     private final static String URL_PREFERENCE_ABOUT_US = "http://berimbasket.ir/about";
     private final static String URL_PREFERENCE_CHANGE_LOG = "http://berimbasket.ir/changelog";
     private final static String URL_PREFERENCE_CONTACT_US = "http://berimbasket.ir/contact-us";
-    private Preference help, aboutUs, terms, changeLog, commentMyket, contactUs, contactDeveloper;
+    private Preference help, aboutUs, terms, changeLog, commentOnStore, contactUs, contactDeveloper;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -41,7 +43,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Prefer
         aboutUs = findPreference(getString(R.string.key_pref_about_us));
         terms = findPreference(getString(R.string.key_pref_terms_and_services));
         changeLog = findPreference(getString(R.string.key_pref_change_log));
-        commentMyket = findPreference(getString(R.string.key_pref_comment_myket));
+        commentOnStore = findPreference(getString(R.string.key_pref_comment_on_store));
         contactUs = findPreference(getString(R.string.key_pref_contact_us));
         contactDeveloper = findPreference(getString(R.string.key_pref_contact_developer));
 
@@ -49,7 +51,10 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Prefer
         aboutUs.setOnPreferenceClickListener(this);
         terms.setOnPreferenceClickListener(this);
         changeLog.setOnPreferenceClickListener(this);
-        commentMyket.setOnPreferenceClickListener(this);
+        if (commentOnStore != null) {
+            // on dev flavor there is no such field
+            commentOnStore.setOnPreferenceClickListener(this);
+        }
         contactUs.setOnPreferenceClickListener(this);
         contactDeveloper.setOnPreferenceClickListener(this);
     }
@@ -77,9 +82,14 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Prefer
             AnalyticsHelper.getInstance().trackEvent(getString(R.string.analytics_category_settings), getString(R.string.analytics_action_change_log), "");
             Redirect.sendToCustomTab(getActivity(), URL_PREFERENCE_CHANGE_LOG);
             return true;
-        } else if (key.equals(commentMyket.getKey())) {
-            AnalyticsHelper.getInstance().trackEvent(getString(R.string.analytics_category_settings), getString(R.string.analytics_action_comment_myket), "");
-            Redirect.sendToMyketForComment(getActivity());
+        } else if (key.equals(commentOnStore.getKey())) {
+            if (BuildConfig.FLAVOR.equals("myket")) {
+                AnalyticsHelper.getInstance().trackEvent(getString(R.string.analytics_category_settings), getString(R.string.analytics_action_comment_myket), "");
+                Redirect.sendToMyketForComment(getActivity());
+            } else if (BuildConfig.FLAVOR.equals("bazaar")) {
+                Toast.makeText(getContext(), "Implement this", Toast.LENGTH_SHORT).show();
+                // TODO: 3/17/2018 implement this suit with bazaar intents
+            }
         } else if (key.equals(contactUs.getKey())) {
             Redirect.sendToCustomTab(getActivity(), URL_PREFERENCE_CONTACT_US);
         } else if (key.equals(contactDeveloper.getKey())) {
